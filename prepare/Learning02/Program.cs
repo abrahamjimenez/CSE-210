3b5d7f7b50a71319a8f8ec68e4e0ec9b496a04e5
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-04-28 14:55:56 -0600

    added class Job, job1, and job2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,9 +2,30 @@ using System;
 
 class Program
 {
+    public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+    }
+
     static void Main(string[] args)
     {
-        // Abstraction Learning Activity
-        Console.WriteLine("Hello Develop02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._company = "Samsung";
+        job2._jobTitle = "Janitor";
+        job2._startYear = 2019;
+        job2._endYear = 2023;
+
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
     }
+
 }
