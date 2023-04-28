c08c7b202c495a84e77ae65f914266f4c686f84d
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-04-28 15:19:09 -0600

    saving. finish step 5

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Job
+{
+
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Dispay()
+    {
+        //Step 5
+        Console.WriteLine($"");
+    }
+}
+
