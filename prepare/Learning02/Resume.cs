1182916fef12482ca2dd858d6ef5f7d891212e99
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-04-28 21:11:41 -0600

    Created file & added code

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    public string _personName;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine(_personName);
+        Console.WriteLine("Jobs:");
+
+        foreach (Job i in _jobs)
+        {
+            i.Display();
+        }
+    }
+}
