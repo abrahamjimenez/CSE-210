0d39c9e8741f1f6e7cf42693079b21895d4c8b9b
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-08 09:30:26 -0600

    added entry.cs code

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,28 @@
+
+namespace Develop02
+{
+    /// <summary>
+    /// The responsibility of an entry is to hold a prompt, response, and date.
+    /// </summary>
+
+    public class Entry
+    {
+        public string prompt;
+        public string response;
+        public string date;
+
+        public string ConvertToString()
+        {
+            return $"{date}\n{prompt}\n{response}\n";
+        }
+
+        public void Hold(string prompt, string response, string date)
+        {
+            this.prompt = prompt;
+            this.response = response;
+            this.date = date;
+        }
+
+    }
+
+}
