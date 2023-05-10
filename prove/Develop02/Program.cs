994ab61f75b47342e6548c4272b0cae202a37a41
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-10 10:22:51 -0600

    added Prompts.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,15 +7,19 @@ namespace Develop02
     {
         static void Main(string[] args)
         {
-            Entry myEntry = new Entry();
-            myEntry.Hold("What is your name?", "Abraham Jimenez", "8 May 2023");
+            Prompts myPrompts = new Prompts();
+            string prompt = myPrompts.GetRandomPrompt();
+            //string response = Console.ReadLine();
 
-            Entry yourEntry = new Entry();
-            yourEntry.Hold("What is your name?", "Abraham Joe", "8 May 2023");
+            Entry myEntry = new Entry();
+            myEntry.Hold(prompt, "Abraham Jimenez", "8 May 2023");
+
+            //Entry yourEntry = new Entry();
+            //yourEntry.Hold(prompt, response, "8 May 2023");
 
             Journal journal = new Journal();
             journal.AddEntry(myEntry);
-            journal.AddEntry(yourEntry);
+            //journal.AddEntry(yourEntry);
 
             List<Entry> entries = journal.GetAllEntries();
             foreach (Entry entry in entries)
---
+++ prove/Develop02/Prompts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Develop02
+{
+	/// <summary>
+	/// The responsibility of this class is to store predefined prompts
+	/// </summary>
+	public class Prompts
+	{
+		public List<String> prompts;
+		public Random number = new Random();
+
+        public Prompts()
+		{
+            prompts = new List<String>();
+            prompts.Add("Who was the most interesting person I interacted with today?");
+            prompts.Add("What was the best part of my day?");
+            prompts.Add("How did I see the hand of the Lord in my life today?");
+            prompts.Add("What is the best part of your day?");
+            prompts.Add("If I had one thing I could do over today, what would it be?");
+        }
+
+		public string GetRandomPrompt()
+		{
+			int randomIndex = number.Next(0, prompts.Count);
+			return prompts[randomIndex];
+		}
+	}
+}
