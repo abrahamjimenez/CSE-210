ed59822ab6e8203fee7a5232197aa736afb7632c
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-13 18:10:00 -0600

    attemtpt 2 — more if’s

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -47,33 +47,6 @@ namespace Develop02
                         string message = entry.ConvertToString();
 
                     }
-
-
-                    // Save to file
-                    string filename = "myFile.txt";
-
-                    using (StreamWriter outputFile = new StreamWriter(filename))
-                    {
-                        //Console.WriteLine("Saving...");
-                        outputFile.WriteLine($"{currentDate} - {prompt} - {userInput}");
-                    }
-
-
-                    // Load file
-                    string[] lines = System.IO.File.ReadAllLines(filename);
-
-                    foreach (string line in lines)
-                    {
-
-                        // Console.WriteLine(line);
-                        // line will have something like this 5/11/2023 - What is the best part of your day? - Everything
-
-                        string[] parts = line.Split("-");
-
-                        //Console.WriteLine("");
-                        //Console.WriteLine("Reading from file...");
-                        //Console.WriteLine(line);
-                    }
                 }
                 else if (input == 2)
                 {
@@ -85,13 +58,46 @@ namespace Develop02
                 }
                 else if (input == 4)
                 {
-                    Console.WriteLine("Saving...");
+                    Console.WriteLine("4");
                 }
                 else
                 {
                     x = false;
                 }
             }
+
+            /// <summary>
+            /// This is where the program continues to start
+            /// </summary>
+
+            //Prompts myPrompts = new Prompts();
+            //string prompt = myPrompts.Display();
+            //Console.WriteLine(prompt);
+            //string userInput = myPrompts.AskInput();
+            //Console.WriteLine("");
+            //string currentDate = myPrompts.DisplayCurrentDate();
+
+            //Entry myEntry = new Entry();
+            //myEntry.Hold(prompt, userInput, currentDate);
+
+            //Journal journal = new Journal();
+            //journal.AddEntry(myEntry);
+
+            //List<Entry> entries = journal.GetAllEntries();
+            //foreach (Entry entry in entries)
+            //{
+            //    string message = entry.ConvertToString();
+
+            //}
+
+            //// Save to file
+            //string filename = "myFile.txt";
+
+            //using (StreamWriter outputFile = new StreamWriter(filename))
+            //{
+            //    Console.WriteLine("Saving...");
+            //    outputFile.WriteLine($"{currentDate} - {prompt} - {userInput}");                
+            //}
 
             //// Load file
             //string[] lines = System.IO.File.ReadAllLines(filename);
