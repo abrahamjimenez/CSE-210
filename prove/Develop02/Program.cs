8ce18bfcfe34c1df684868e7e260d2eea33b7c7f
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-13 18:28:23 -0600

    saving, some errors

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -13,6 +13,7 @@ namespace Develop02
             /// This is where the program will loop through a menu
             /// </summary>
             ///
+
             bool x = true;
 
             while (x)
@@ -47,6 +48,78 @@ namespace Develop02
                         string message = entry.ConvertToString();
 
                     }
+
+                    Console.WriteLine("Please choose one of the following choices");
+                    Console.WriteLine("1. Write");
+                    Console.WriteLine("2. Display");
+                    Console.WriteLine("3. Load");
+                    Console.WriteLine("4. Save");
+                    Console.WriteLine("5. Quit");
+                    int input_two = int.Parse(Console.ReadLine());
+                    Console.WriteLine("");
+
+                    if (input_two == 1)
+                    {
+                        Prompts myPrompts_2 = new Prompts();
+                        string prompt_2 = myPrompts.Display();
+                        Console.WriteLine(prompt);
+                        string userInput_2 = myPrompts.AskInput();
+                        Console.WriteLine("");
+                        string currentDate_2 = myPrompts.DisplayCurrentDate();
+
+                        Entry myEntry_2 = new Entry();
+                        myEntry.Hold(prompt, userInput, currentDate);
+
+                        Journal journal_2 = new Journal();
+                        journal.AddEntry(myEntry);
+
+                        List<Entry> entries_2 = journal.GetAllEntries();
+                        foreach (Entry entry in entries)
+                        {
+                            string message = entry.ConvertToString();
+
+                        }
+                    }
+                    else if (input_two == 2) {
+
+                    }
+                    else if (input_two == 3)
+                    {
+                        string filename = "myFile.txt";
+
+                        // Load file
+                        string[] lines = System.IO.File.ReadAllLines(filename);
+
+                        foreach (string line in lines)
+                        {
+
+                            // Console.WriteLine(line);
+                            // line will have something like this 5/11/2023 - What is the best part of your day? - Everything
+
+                            string[] parts = line.Split("-");
+
+                            Console.WriteLine("");
+                            Console.WriteLine("Reading from file...");
+                            Console.WriteLine(line);
+                        }
+                    }
+                    else if (input_two == 4)
+                    {
+
+                        // Save to file
+                        string filename = "myFile.txt";
+
+                        using (StreamWriter outputFile = new StreamWriter(filename))
+                        {
+                            Console.WriteLine("Saving...");
+                            outputFile.WriteLine($"{currentDate} - {prompt} - {userInput}");
+                        }
+                    }
+                    else if (input_two == 5)
+                    {
+                        x = false;
+                    }
+
                 }
                 else if (input == 2)
                 {
@@ -82,55 +155,6 @@ namespace Develop02
                     x = false;
                 }
             }
-
-            /// <summary>
-            /// This is where the program continues to start
-            /// </summary>
-
-            //Prompts myPrompts = new Prompts();
-            //string prompt = myPrompts.Display();
-            //Console.WriteLine(prompt);
-            //string userInput = myPrompts.AskInput();
-            //Console.WriteLine("");
-            //string currentDate = myPrompts.DisplayCurrentDate();
-
-            //Entry myEntry = new Entry();
-            //myEntry.Hold(prompt, userInput, currentDate);
-
-            //Journal journal = new Journal();
-            //journal.AddEntry(myEntry);
-
-            //List<Entry> entries = journal.GetAllEntries();
-            //foreach (Entry entry in entries)
-            //{
-            //    string message = entry.ConvertToString();
-
-            //}
-
-            //// Save to file
-            //string filename = "myFile.txt";
-
-            //using (StreamWriter outputFile = new StreamWriter(filename))
-            //{
-            //    Console.WriteLine("Saving...");
-            //    outputFile.WriteLine($"{currentDate} - {prompt} - {userInput}");                
-            //}
-
-            //// Load file
-            //string[] lines = System.IO.File.ReadAllLines(filename);
-
-            //foreach (string line in lines)
-            //{
-
-            //    // Console.WriteLine(line);
-            //    // line will have something like this 5/11/2023 - What is the best part of your day? - Everything
-
-            //    string[] parts = line.Split("-");
-
-            //    Console.WriteLine("");
-            //    Console.WriteLine("Reading from file...");
-            //    Console.WriteLine(line);
-            //}
         }
     }
 }
