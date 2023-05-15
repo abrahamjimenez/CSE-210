a4fe6557dd436c6c1ee384c7a36ab12e26dd715d
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-15 09:41:19 -0600

    class practive

---
+++ prove/Develop03/Person.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//namespace Develop03
+//{
+//    public class Account
+//    {
+//        // Getter
+//        private int _number;
+
+//        public Account(int number)
+//        {
+//            SetNumber(number);
+//        }
+
+//        public int GetNumber()
+//        {
+//            return _number;
+//        }
+
+//        // Setter
+//        private void SetNumber(int number)
+//        {
+//            if (number < 0)
+//            {
+//                throw new ArgumentException("Account number must be positive. ");
+//            }
+
+//            _number = number;
+//        }
+//    }
+//}
+
