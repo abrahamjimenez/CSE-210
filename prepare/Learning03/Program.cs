a544257c14b074487d28160cee1098e505cde1b3
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-17 15:52:05 -0600

    new assignment for fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,31 +2,5 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Guess the magic number!");
-        Console.WriteLine("");
-        Random random_number = new Random();
-        int magic_number = random_number.Next(1, 101);
-
-
-        Console.WriteLine("What is your guess?");
-        int user_guess = int.Parse(Console.ReadLine());
-
-        while (user_guess != magic_number)
-        {
-
-            if (user_guess > magic_number)
-            {
-                Console.WriteLine("Lower:");
-                user_guess = int.Parse(Console.ReadLine());
-            }
-            else if (user_guess < magic_number)
-            {
-                Console.WriteLine("Higher:");
-                user_guess = int.Parse(Console.ReadLine());
-            }
-        }
-        Console.Write("You guessed it!");
-    }
+    
 }
