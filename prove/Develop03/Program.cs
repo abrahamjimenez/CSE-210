1bef8f1eeff298eb4b9cc63db50c8a514634404c
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-17 09:42:48 -0600

    moved files around to correct projects

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,29 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Guess the magic number!");
-        Console.WriteLine("");
-        Random random_number = new Random();
-        int magic_number = random_number.Next(1, 101);
-
-
-        Console.WriteLine("What is your guess?");
-        int user_guess = int.Parse(Console.ReadLine());
-
-        while (user_guess != magic_number)
-        {
-
-            if (user_guess > magic_number)
-            {
-                Console.WriteLine("Lower:");
-                user_guess = int.Parse(Console.ReadLine());
-            }
-            else if (user_guess < magic_number)
-            {
-                Console.WriteLine("Higher:");
-                user_guess = int.Parse(Console.ReadLine());
-            }
-        }
-        Console.Write("You guessed it!");
+        Console.WriteLine("Hello Develop03 World!");
     }
 }
