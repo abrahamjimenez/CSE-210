13ed375009f16237c65ad12e50f0d832295c4659
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-23 12:31:20 -0600

    starting over to understand better

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Linq;
-using Develop03;
+﻿using Develop03;
 
-class Program
+class Program_2
 {
     static void Main(string[] args)
     {
-        string verse = "For behold, this is my work and my glory to bring to pass the immortality and eternal life of man.";
-
-        Verse myVerse = new Verse(verse); // Create an instance of the Verse class, passing the verse as an argument to the constructor
-        myVerse.MaskRandomWords(); // Call the MaskRandomWords method to start the word masking process
+        
     }
 }
--- prove/Develop03/Words.cs
+++ prove/Develop03/Words.cs
@@ -1,39 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace Develop03
 {
-    /// <summary>
-    /// Holds a word 
-    /// </summary>
-    public class Words
-    {
-        private string[] words; // Private field to store the words
-
-        public Words(string verse)
-        {
-            words = verse.Split(" "); // Split the verse into individual words and store them in the words array
-        }
-
-        public int GetLength()
-        {
-            return words.Length; // Return the length of the words array
-        }
-
-        public bool IsWordMasked(int index)
-        {
-            return words[index] == "___"; // Check if the word at the given index is already masked (replaced with '___')
-        }
-
-        public void MaskWord(int index)
-        {
-            words[index] = "___"; // Mask the word at the given index by replacing it with '___'
-        }
-
-        public override string ToString()
-        {
-            return string.Join(" ", words); // Return the entire verse by joining the words with spaces
-        }
-    }
+	public class Words
+	{
+		public Words()
+		{
+		}
+	}
 }
+
