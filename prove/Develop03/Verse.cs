d9dc6fd5c48de9825411a4ad863330090a63edd9
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-05-25 22:00:37 -0600

    will figure out how to keep changing

--- prove/Develop03/Verse.cs
+++ prove/Develop03/Verse.cs
@@ -22,7 +22,7 @@ namespace Develop03
 			List <string> wordList = new List<string>();
 
             // Hold scripture verses in here and output them
-            verse = "For behold, this is my work and my glory-to bring to pass the immortality and eternal life of man.";
+            verse = "For behold, this is my work and my glory to bring to pass the immortality and eternal life of man.";
 			verse2 = "Ye are the light of the world. A city that is set on an hill cannot be hid. Neither do men light a candle, and put it under a bushel, but on a candlestick; and it giveth light unto all that are in the house.Let your light so shine before men, that they may see your good works, and glorify your Father which is in heaven.";
 
             wordList.Add(verse);
--- prove/Develop03/Words.cs
+++ prove/Develop03/Words.cs
@@ -11,46 +11,57 @@ namespace Develop03
         string[] word;
         bool visible;
 
-        int randomNumber;
-        int rnd;
-
-        // Delete
-        int x = 0;
+        int replacedCount;
 
         public Words(string words)
         {
             word = words.Split(" ");
         }
 
-        public string SetInvisible()
+        public void SetInvisible()
         {
             Random random = new Random();
-            int rnd = random.Next(0, word.Length); // This gets a random int in word
-                                                   //Console.WriteLine(word[rnd]); // This outputs a random word
 
             // Put this code in a while loop to keep the code running
-            while (x - 1 != 10) // While the verse length and '___' are not the same number...
+            while (replacedCount < word.Length) // While the verse length and '___' are not the same number...
             {
-                Console.WriteLine(x);
-                x++;
+                int rnd = random.Next(0, word.Length); // This gets a random int in word
+
+                // Get length of word to replace with number of '___'
+                int invisibleNum = word[rnd].Length;
+                string replaceWithUnderscores = String.Concat(Enumerable.Repeat("_", invisibleNum)); // This writes '_' as much as the words length
+
+                // Ask for input
+                Console.WriteLine("Press enter to continue");
+                Console.ReadLine();
+
+                // use if statement to check
+                while (word[rnd] != replaceWithUnderscores && !word[rnd].Contains("_") && word[rnd] != null)
+                {
+                    Console.WriteLine($"-YES: {word[rnd]}-");
+                    word[rnd] = replaceWithUnderscores;
+                    replacedCount++;
+                    Console.WriteLine("runs1");
+                }
+
+                Console.WriteLine("runs2");
+                // This code is not done yet. Now I have to get the whole sentence back together, but instead of the word, it needs to have ___
+                //Console.WriteLine(); // Todo: add later
+                //word[rnd] = replaceWithUnderscores; // Replaces word with underscores
+                string joinWords = string.Join(" ", word); // This joins the words back together but with the new "_"
+
+                // Works Perfect! Now I need to add a Console.ReadLine() That takes input. Once the input is taken, this code will keep running
+                // I need to add a check however, that checks if the word has been replaced with dashes ('_') or not
+
+                //Console.WriteLine($"1. {rnd}"); // rnd is the index of the word that is changed
+                Console.WriteLine($"1. {joinWords}");
+                //return joinWords;
             }
-
-            // Get length of word to replace with number of '___'
-            int invisibleNum = word[rnd].Length;
-            string replaceWithUnderscores = String.Concat(Enumerable.Repeat("_", invisibleNum)); // This writes '_' as much as the words length
-
-            // This code is not done yet. Now I have to get the whole sentence back together, but instead of the word, it needs to have ___
-            Console.WriteLine();
-            word[rnd] = replaceWithUnderscores; // Replaces word with underscores
-            string joinWords = string.Join(" ", word); // This joins the words back together but with the new "_"
-
-            return joinWords;
         }
 
         public void GetInvisible()
         {
-            string invisibleWord = SetInvisible();
-            Console.WriteLine(invisibleWord);
+            SetInvisible();
         }
     }
 }
