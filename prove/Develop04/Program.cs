80ff257d9b02760ae56c8ddaf5fa0cdd1cea7bc4
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-06-20 12:08:11 -0600

    Added - Menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -6,14 +6,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Breathing_Activity breathing_Activity = new Breathing_Activity();
-        //breathing_Activity.RunBreathingActivity();
+        bool quit = false;
 
-        //Reflection_Activity reflection_Activity = new Reflection_Activity();
-        //reflection_Activity.RunReflectionActivity();
+        while (!quit)
+        {
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
 
-        Listing_Activity listing_Activity = new Listing_Activity();
-        listing_Activity.RunListingActivity();
+            Console.Write("Select a choice from the menu: ");
+            string input = Console.ReadLine();
 
+            if (input == "1")
+            {
+                Breathing_Activity breathing_Activity = new Breathing_Activity();
+                breathing_Activity.RunBreathingActivity();
+            }
+            else if (input == "2")
+            {
+                Reflection_Activity reflection_Activity = new Reflection_Activity();
+                reflection_Activity.RunReflectionActivity();
+            }
+            else if (input == "3")
+            {
+                Listing_Activity listing_Activity = new Listing_Activity();
+                listing_Activity.RunListingActivity();
+            }
+            else if (input == "4")
+            {
+                quit = true;
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+        }
     }
 }
