559d6fb047e69518c765bdc38eaaed2968e98fa5
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-06-21 13:09:18 -0600

    Added - Foundation

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Learning05
+{
+	public class Shape
+	{
+		public Shape()
+		{
+
+		}
+
+		public string GetColor()
+		{
+			return "";
+		}
+
+		public string SetColor(string color)
+		{
+			return color;
+		}
+
+		public virtual void GetOverride()
+		{
+
+		}
+	}
+}
+
